8c2311a04df1a124e670e2e937907091731a5b88
Randy Burgess <wrburgess@wrburgess.com>
2012-09-07 10:18:05 -0500

    added Player specs and attr validation

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,7 @@
 class Player < ActiveRecord::Base
   attr_accessible :first_name, :last_name
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
 
   def full_name
     "#{first_name} #{last_name}"
