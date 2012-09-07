1442a6949a030191a357f5dcd3dd149a2ff351f0
Randy Burgess <wrburgess@wrburgess.com>
2012-09-07 09:58:02 -0500

    fixed broken factory

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,7 @@
 class Player < ActiveRecord::Base
   attr_accessible :first_name, :last_name
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Player do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has a valid factory' do
+    FactoryGirl.create(:player).should be_valid
+  end
+
+  describe '#full_name' do
+
+    it 'sets full_name from first_name and last_name' do
+      FactoryGirl.create(:player).full_name.should == 'John Smith'
+    end
+
+  end
+
 end
