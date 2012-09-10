8022449c813416f231c7aa2eddde429afcf026aa
Randy Burgess <wrburgess@wrburgess.com>
2012-09-09 20:58:01 -0500

    changed roster_manager model to roster_user

--- spec/models/group_user_spec.rb
+++ spec/models/group_user_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe GroupUser do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has a valid factory' do
+    FactoryGirl.create(:group_user).should be_valid
+  end
+
+  it 'is invalid without a user_id' do
+    FactoryGirl.build(:group_user, user_id: nil).should_not be_valid
+  end
+
+  it 'is invalid without a group_id' do
+    FactoryGirl.build(:group_user, group_id: nil).should_not be_valid
+  end
+
+  it { should belong_to(:user) }
+  it { should belong_to(:group) }
+
 end
