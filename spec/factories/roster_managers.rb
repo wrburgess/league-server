8aa0329c984f9729cace29bce1db1e51a7d0ef5b
Randy Burgess <wrburgess@wrburgess.com>
2012-09-09 20:44:57 -0500

    changed GroupManager and RosterManager to Users

--- spec/factories/roster_managers.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :roster_manager do
-    user_id 1
-    roster_id 1
-  end
-end
---
+++ spec/factories/roster_users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :roster_user do
+    user_id 1
+    roster_id 1
+  end
+end
