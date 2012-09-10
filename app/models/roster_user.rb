1ccc22dc57bb87d8c9be66ab3d015c6452840f63
Randy Burgess <wrburgess@wrburgess.com>
2012-09-09 20:58:01 -0500

    changed roster_manager model to roster_user

--- app/models/roster_user.rb
+++ app/models/roster_user.rb
@@ -1,8 +1,8 @@
-class RosterManager < ActiveRecord::Base
+class RosterUser < ActiveRecord::Base
   attr_accessible :roster_id, :user_id
 
   validates_presence_of :roster_id, :user_id
-  
+
   belongs_to :users
   belongs_to :groups
 end
