f892d7c97c7fb3239d3330223bd7f44f6b0147e3
Randy Burgess <wrburgess@wrburgess.com>
2012-09-09 23:00:02 -0500

    setting up an apicontroller

---
+++ app/controllers/api/v1/players_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::PlayersController < ApiController
+
+  def index
+    # render json: Player.all
+  end
+
+  def show
+    # render json: Player.find_by_id(params[:id])
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+end
