8433d85889adcd666aec641bdaa61c8a79c64a30
Randy Burgess <wrburgess@wrburgess.com>
2012-09-13 20:28:23 -0500

    added player admin views, automated seeds

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,17 @@ League::Application.routes.draw do
   devise_for :users
 
   namespace :admin do
-    resources :users do
-      get 'users' => 'users#index'
+    resources :players do
+      get 'players' => 'players#index'
+      get 'players/:id' => 'players#show'
     end
 
     resources :teams do
       get 'teams' => 'teams#index'
+    end
+
+    resources :users do
+      get 'users' => 'users#index'
     end
   end
 
