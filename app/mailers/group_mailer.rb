cee8e08007b28eec9d3303c89974bade9a67060d
Randy Burgess <wrburgess@wrburgess.com>
2012-09-22 16:04:17 -0500

    continued work on mailer setup

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -1,3 +1,13 @@
 class GroupMailer < ActionMailer::Base
-  default from: "from@example.com"
+  layout 'mailer_default'
+
+  def weekly_review(user)
+    body          { user: user, url: "http://example.com/login" }
+    charset       "utf-8"
+    content_type  "text/html"
+    from          Settings.mailer.from
+    recipients    user.email
+    sent_on       Time.now
+    subject       "Group Weekly Review"
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 League::Application.initialize!
+
+# Designate ActionMailer to recognize HAML files
+ActionMailer::Base.register_template_extension('haml')
