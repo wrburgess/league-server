efd7123810c8e7fe0e2e57087880ddb1542db93c
Randy Burgess <wrburgess@wrburgess.com>
2012-10-27 17:41:17 -0500

    annotate

--- app/models/api_log.rb
+++ app/models/api_log.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: api_logs
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ApiLog < ActiveRecord::Base
   # attr_accessible :title, :body
 end
--- spec/models/api_log_spec.rb
+++ spec/models/api_log_spec.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: api_logs
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe ApiLog do
