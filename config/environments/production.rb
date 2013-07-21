b4c07dd0c24d28a5cee8a144b09c6bdc39ebaec1
Randy Burgess <wrburgess@wrburgess.com>
2013-07-21 10:44:01 -0500

    removing auto explain setting from env files for Rails 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,6 @@ League::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   # For Devise
   config.action_mailer.default_url_options = { :host => 'something heroku?' }
 
