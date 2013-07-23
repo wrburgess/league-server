ee4c23bfee2323eda0aec771c40b460832179358
Randy Burgess <wrburgess@wrburgess.com>
2013-07-22 19:52:37 -0500

    reconfig of player controller for admin section

--- app/controllers/admin/players_controller.rb
+++ app/controllers/admin/players_controller.rb
@@ -1,20 +1,19 @@
 class Admin::PlayersController < AdminController
-  protect_from_forgery
 
   def index
     @players = Player.all
-
-    @page_title = "Players Index"
-    @body_id = "players-index"
-    @body_class = "page"
   end
 
   def show
     @player = Player.find_by_id(params[:id])
+  end
 
-    @page_title = "Player Show"
-    @body_id = "players-show"
-    @body_class = "page"
+  def edit
+    @player = Player.find_by_id(params[:id])
+  end
+
+  def new
+    @player = Player.new
   end
 
 end
