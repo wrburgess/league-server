2f5352533479eb7a82031ad696017270454969a1
Randy Burgess <wrburgess@wrburgess.com>
2013-07-25 11:09:27 -0500

    adding integration tests for admin_player new and edit forms

--- spec/features/admin/player_crud.rb
+++ spec/features/admin/player_crud.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
-describe "Admin Player CRUD" do
+feature "Admin Player CRUD" do
 
   before do
+    player_position1 = FactoryGirl.create(:player_position, name: "Guard", abbreviation: "G")
+    player_position2 = FactoryGirl.create(:player_position, name: "Center", abbreviation: "C")
+    player_position3 = FactoryGirl.create(:player_position, name: "Forward", abbreviation: "F")
+    player_team1 = FactoryGirl.create(:player_team, location_name: "Georgia", abbreviation: "GA")
+    player_team2 = FactoryGirl.create(:player_team, location_name: "Florida", abbreviation: "FLA")
+    player_team3 = FactoryGirl.create(:player_team, location_name: "Alabama", abbreviation: "ALA")
     @player1 = FactoryGirl.create(:player)
     @player2 = FactoryGirl.create(:player)
     @player3 = FactoryGirl.create(:player)
@@ -28,12 +34,39 @@ describe "Admin Player CRUD" do
       visit new_admin_player_path
       page.should have_css("body.players.new")
     end
+
+    it "adds a new player, redirects to admin_player show view" do
+      visit new_admin_player_path
+      fill_in "player[first_name]", with: "Todd"
+      fill_in "player[last_name]", with: "Gurley"
+      select "FLA", from: "player[player_team_id]"
+      select "G", from: "player[player_position_id]"
+      click_button "Create Player"
+      player = Player.where(first_name: "Todd", last_name: "Gurley")
+      expect(player.count).to eq(1)
+      current_path.should == admin_player_path(player.first)
+    end
   end
 
   describe "#edit" do
     it "has the correct default elements" do
       visit edit_admin_player_path(@player1)
       page.should have_css("body.players.edit")
+    end
+
+    it "edits an existing player, redirects to admin_player show view" do
+      visit edit_admin_player_path(@player1)
+      fill_in "player[first_name]", with: "Aaron"
+      fill_in "player[last_name]", with: "Murray"
+      select "ALA", from: "player[player_team_id]"
+      select "G", from: "player[player_position_id]"
+      click_button "Update Player"
+      @player1.reload
+      expect(@player1.first_name).to eq("Aaron")
+      expect(@player1.last_name).to eq("Murray")
+      expect(@player1.player_position.abbreviation).to eq("G")
+      expect(@player1.player_team.abbreviation).to eq("ALA")
+      current_path.should == admin_player_path(@player1)
     end
   end
 
