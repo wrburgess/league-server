012999a726e8b0a6af298f97b21e5b525cedce32
Randy Burgess <wrburgess@wrburgess.com>
2013-07-27 10:57:08 -0500

    refactoring admin player crud feature specs for speed

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe PlayersController do
-  let(:group) { FactoryGirl.create(:group) }
-  let(:roster) { FactoryGirl.create(:roster) }
+  let!(:group) { FactoryGirl.create(:group) }
+  let!(:roster) { FactoryGirl.create(:roster) }
   let(:player1) { FactoryGirl.create(:player) }
   let(:player2) { FactoryGirl.create(:player) }
   let(:player3) { FactoryGirl.create(:player) }
@@ -10,14 +10,12 @@ describe PlayersController do
 
   describe "#show" do
     it "renders the Player show view" do
-      roster
       get :show, id: player2, group_id: group
       response.should render_template :show
       response.should render_template layout: "layouts/browser"
     end
 
     it "assigns the requested player to @player" do
-      roster
       get :show, id: player2, group_id: group
       expect(assigns(player2)).to eq @player2
     end
--- spec/features/admin/player_crud_spec.rb
+++ spec/features/admin/player_crud_spec.rb
@@ -1,46 +1,35 @@
 require 'spec_helper'
 
 feature "Admin Player CRUD" do
-
+  let!(:player_position1) { FactoryGirl.create(:player_position, name: "Guard", abbreviation: "G") }
+  let!(:player_position2) { FactoryGirl.create(:player_position, name: "Center", abbreviation: "C") }
+  let!(:player_position3) { FactoryGirl.create(:player_position, name: "Forward", abbreviation: "F") }
+  let!(:player_team1) { FactoryGirl.create(:player_team, location_name: "Georgia", abbreviation: "GA") }
+  let!(:player_team2) { FactoryGirl.create(:player_team, location_name: "Florida", abbreviation: "FLA") }
+  let!(:player_team3) { FactoryGirl.create(:player_team, location_name: "Alabama", abbreviation: "ALA") }
   let(:player1) { FactoryGirl.create(:player) }
-  let(:player2) { FactoryGirl.create(:player) }
-  let(:player3) { FactoryGirl.create(:player) }
-  let(:player4) { FactoryGirl.create(:player) }
-
-  before do
-    player_position1 = FactoryGirl.create(:player_position, name: "Guard", abbreviation: "G")
-    player_position2 = FactoryGirl.create(:player_position, name: "Center", abbreviation: "C")
-    player_position3 = FactoryGirl.create(:player_position, name: "Forward", abbreviation: "F")
-    player_team1 = FactoryGirl.create(:player_team, location_name: "Georgia", abbreviation: "GA")
-    player_team2 = FactoryGirl.create(:player_team, location_name: "Florida", abbreviation: "FLA")
-    player_team3 = FactoryGirl.create(:player_team, location_name: "Alabama", abbreviation: "ALA")
-  end
+  let(:player2) { FactoryGirl.create(:player, first_name: "Todd", last_name: "Gurley") }
+  let(:player3) { FactoryGirl.create(:player, first_name: "Keith", last_name: "Marshall") }
 
   describe "#index" do
-    it "has the correct default elements" do
-      visit admin_players_path
-      expect(page).to have_css "body.players.index"
-    end
-
-    it "has the correct content" do
+    it "has the correct content and elements" do
       player1
       player2
       player3
       visit admin_players_path
+      expect(page).to have_css "body.players.index"
       expect(page).to have_content "Hines"
       expect(page).to have_content "Ward"
+      expect(page).to have_content "Keith"
+      expect(page).to have_content "Marshall"
       expect(page).to_not have_content "Bob"
     end
   end
 
   describe "#show" do
-    it "has the correct default elements" do
+    it "has the correct content and elements" do
       visit admin_player_path(player1)
       expect(page).to have_css "body.players.show"
-    end
-
-    it "has the correct content" do
-      visit admin_player_path(player1)
       expect(page).to have_content "Hines"
       expect(page).to have_content "Ward"
       expect(page).to have_content "GA"
@@ -50,38 +39,34 @@ feature "Admin Player CRUD" do
   end
 
   describe "#new" do
-    it "has the correct default elements" do
-      visit new_admin_player_path
-      expect(page).to have_css "body.players.new" 
-    end
-
     it "adds a new player, redirects to admin_player show view" do
       visit new_admin_player_path
+      expect(page).to have_css "body.players.new"
+
       fill_in "player[first_name]", with: "Todd"
       fill_in "player[last_name]", with: "Gurley"
       select "FLA", from: "player[player_team_id]"
       select "G", from: "player[player_position_id]"
       click_button "Create Player"
       player = Player.where(first_name: "Todd", last_name: "Gurley")
+
       expect(player.count).to eq 1
       expect(current_path).to eq admin_player_path player.first
     end
   end
 
   describe "#edit" do
-    it "has the correct default elements" do
-      visit edit_admin_player_path player1
-      expect(page).to have_css "body.players.edit"
-    end
-
     it "edits an existing player, redirects to admin_player show view" do
       visit edit_admin_player_path player1
+      expect(page).to have_css "body.players.edit"
+      
       fill_in "player[first_name]", with: "Aaron"
       fill_in "player[last_name]", with: "Murray"
       select "ALA", from: "player[player_team_id]"
       select "G", from: "player[player_position_id]"
       click_button "Update Player"
       player1.reload
+
       expect(player1.first_name).to eq "Aaron"
       expect(player1.last_name).to eq "Murray"
       expect(player1.player_position.abbreviation).to eq "G"
