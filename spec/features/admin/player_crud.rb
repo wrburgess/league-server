96c9c12a30096047bef23bb58b91de35b15484cc
Randy Burgess <wrburgess@wrburgess.com>
2013-07-26 21:15:15 -0500

    cleaning up syntax on player_crud feature spec

--- spec/features/admin/player_crud.rb
+++ spec/features/admin/player_crud.rb
@@ -19,21 +19,40 @@ feature "Admin Player CRUD" do
   describe "#index" do
     it "has the correct default elements" do
       visit admin_players_path
-      page.should have_css("body.players.index")
+      expect(page).to have_css "body.players.index"
+    end
+
+    it "has the correct content" do
+      player1
+      player2
+      player3
+      visit admin_players_path
+      expect(page).to have_content "Hines"
+      expect(page).to have_content "Ward"
+      expect(page).to_not have_content "Bob"
     end
   end
 
   describe "#show" do
     it "has the correct default elements" do
       visit admin_player_path(player1)
-      page.should have_css("body.players.show")
+      expect(page).to have_css "body.players.show"
+    end
+
+    it "has the correct content" do
+      visit admin_player_path(player1)
+      expect(page).to have_content "Hines"
+      expect(page).to have_content "Ward"
+      expect(page).to have_content "GA"
+      expect(page).to have_content "Center"
+      expect(page).to_not have_content "Bob"
     end
   end
 
   describe "#new" do
     it "has the correct default elements" do
       visit new_admin_player_path
-      page.should have_css("body.players.new")
+      expect(page).to have_css "body.players.new" 
     end
 
     it "adds a new player, redirects to admin_player show view" do
@@ -44,30 +63,30 @@ feature "Admin Player CRUD" do
       select "G", from: "player[player_position_id]"
       click_button "Create Player"
       player = Player.where(first_name: "Todd", last_name: "Gurley")
-      expect(player.count).to eq(1)
-      current_path.should == admin_player_path(player.first)
+      expect(player.count).to eq 1
+      expect(current_path).to eq admin_player_path player.first
     end
   end
 
   describe "#edit" do
     it "has the correct default elements" do
-      visit edit_admin_player_path(player1)
-      page.should have_css("body.players.edit")
+      visit edit_admin_player_path player1
+      expect(page).to have_css "body.players.edit"
     end
 
     it "edits an existing player, redirects to admin_player show view" do
-      visit edit_admin_player_path(player1)
+      visit edit_admin_player_path player1
       fill_in "player[first_name]", with: "Aaron"
       fill_in "player[last_name]", with: "Murray"
       select "ALA", from: "player[player_team_id]"
       select "G", from: "player[player_position_id]"
       click_button "Update Player"
       player1.reload
-      expect(player1.first_name).to eq("Aaron")
-      expect(player1.last_name).to eq("Murray")
-      expect(player1.player_position.abbreviation).to eq("G")
-      expect(player1.player_team.abbreviation).to eq("ALA")
-      current_path.should == admin_player_path(player1)
+      expect(player1.first_name).to eq "Aaron"
+      expect(player1.last_name).to eq "Murray"
+      expect(player1.player_position.abbreviation).to eq "G"
+      expect(player1.player_team.abbreviation).to eq "ALA"
+      expect(current_path).to eq admin_player_path player1
     end
   end
 
