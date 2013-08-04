97386fbebbb80044e5be458a1f344225a7dd88f3
Randy Burgess <wrburgess@wrburgess.com>
2013-08-04 14:36:27 -0500

    finishing basics for user viewing group logs. Closes #19

--- spec/features/group_transactions_spec.rb
+++ spec/features/group_transactions_spec.rb
@@ -2,28 +2,19 @@ require 'spec_helper'
 
 describe "Group Transactions Page" do
   let!(:group) { FactoryGirl.create :group }
-  # let!(:player1) { FactoryGirl.create :player }
-  # let!(:player2) { FactoryGirl.create :player }
-  # let!(:player3) { FactoryGirl.create :player }
-  # let!(:player4) { FactoryGirl.create :player }
-  # let!(:player5) { FactoryGirl.create :player }
-  # let!(:player6) { FactoryGirl.create :player }
-  # let!(:player7) { FactoryGirl.create :player }
-  # let!(:player8) { FactoryGirl.create :player }
-  # let!(:player9) { FactoryGirl.create :player }
-  # let!(:player10) { FactoryGirl.create :player }
-  # let(:roster1) { FactoryGirl.create :roster, name: "Illinois Towers" }
-  # let(:roster2) { FactoryGirl.create :roster, name: "St. Simons Avians" }
-  # let(:roster3) { FactoryGirl.create :roster, name: "Orlando Thunder" }
-  # let(:roster4) { FactoryGirl.create :roster, name: "Fairfax Pitbulls" }
-  # let(:roster5) { FactoryGirl.create :roster, name: "Des Moines Panthers" }
-  # let(:roster6) { FactoryGirl.create :roster, name: "Golden Isles PhishHeads" }
-  # let(:roster7) { FactoryGirl.create :roster, name: "Norfolk Dogs" }
-  # let(:roster8) { FactoryGirl.create :roster, name: "Northern Virginia River Rats" }
-  let!(:group_log1) { FactoryGirl.create :group_log, group: group }
-  let!(:group_log2) { FactoryGirl.create :group_log, group: group }
-  let!(:group_log3) { FactoryGirl.create :group_log, group: group }
-  let!(:group_log4) { FactoryGirl.create :group_log, group: group }
+  let(:roster1) { FactoryGirl.create :roster, name: "Illinois Towers" }
+  let(:roster2) { FactoryGirl.create :roster, name: "St. Simons Avians" }
+  let(:roster3) { FactoryGirl.create :roster, name: "Orlando Thunder" }
+  let(:roster4) { FactoryGirl.create :roster, name: "Fairfax Pitbulls" }
+  let(:roster5) { FactoryGirl.create :roster, name: "Des Moines Panthers" }
+  let(:roster6) { FactoryGirl.create :roster, name: "Golden Isles PhishHeads" }
+  let(:roster7) { FactoryGirl.create :roster, name: "Norfolk Dogs" }
+  let(:roster8) { FactoryGirl.create :roster, name: "Northern Virginia River Rats" }
+
+  let!(:group_log1) { FactoryGirl.create :group_log, group: group, roster: roster1 }
+  let!(:group_log2) { FactoryGirl.create :group_log, group: group, roster: roster4 }
+  let!(:group_log3) { FactoryGirl.create :group_log, group: group, roster: roster6 }
+  let!(:group_log4) { FactoryGirl.create :group_log, group: group, roster: roster8, group_owner: true }
 
   describe "#transactions" do
     it "has the correct default elements", :slow do
@@ -32,16 +23,21 @@ describe "Group Transactions Page" do
       page.should have_css("h1", :text => "Transactions")
     end
 
-    xit "redirects to first team group_roster show view", :slow do
-      visit group_standings_path(group, roster3)
-      find("tr.row-#{roster2.id} td.name a").click
-      expect(current_path).to eq group_roster_path group, roster2
+    it "redirects to first team group_roster show view", :slow do
+      visit group_transactions_path(group, roster3)
+      find("tr.row-#{group_log1.id} td.team-name a").click
+      expect(current_path).to eq group_roster_path group, group_log1.roster
     end
 
-    xit "redirects to player show view", :slow do
-      visit group_standings_path(group, roster6)
-      find("tr.row-#{transaction5.id} td.player a").click
-      expect(current_path).to eq group_player_path group, player
+    it "inserts a commish flag for commish-executed group logs", :slow do
+      visit group_transactions_path(group, roster3)
+      page.should have_css("tr.row-#{group_log4.id}.commish-action")
+    end
+
+    it "redirects to player show view", :slow do
+      visit group_transactions_path(group, roster6)
+      find("tr.row-#{group_log2.id} td.player-name a").click
+      expect(current_path).to eq group_player_path group, group_log2.player
     end
   end
 end
