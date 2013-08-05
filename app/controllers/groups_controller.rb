4c7d076ea2d3eeff5aa44a4716310c57625dc7a5
Randy Burgess <wrburgess@wrburgess.com>
2013-08-05 10:00:40 -0500

    refactoring groups_controller to take advantage of browser_controller before_filter

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,20 +1,19 @@
 class GroupsController < BrowserController
 
   def draft
-    @group = Group.find(params[:group_id])
+    
   end
 
   def show
-    @group = Group.find(params[:group_id])
+
   end
 
   def standings
-    @group = Group.find(params[:group_id])
+
   end
 
   def transactions
-    group = Group.find(params[:group_id])
-    @group_logs = group.group_logs.includes(:player, :roster).all
+    @group_logs = @group.group_logs.includes(:player, :roster)
   end
 
 end
