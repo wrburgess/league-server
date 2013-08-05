46b25310879006a7e830aad62dc897fcc78c2c97
Randy Burgess <wrburgess@wrburgess.com>
2013-08-05 08:48:25 -0500

    resolving n+1 problem on transactions view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,7 +13,8 @@ class GroupsController < BrowserController
   end
 
   def transactions
-    @group = Group.find(params[:group_id])
+    group = Group.find(params[:group_id])
+    @group_logs = group.group_logs.includes(:player, :roster).all
   end
 
 end
